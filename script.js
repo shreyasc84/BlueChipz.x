e12a3ca31b226723167f5cb1201439cc1ca4cd19
shreyasc84 <shreyasc84@gmail.com>
2025-01-25 21:57:29 +0530

    v2

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ document.addEventListener('DOMContentLoaded', () => {
         menu.classList.toggle('hidden');
     });
 });
+
+// Function to load the navigation menu
+function loadNavigationMenu() {
+    // Fetch the navbar.html file
+    fetch('nav.html')
+        .then(response => response.text())
+        .then(data => {
+            // Insert the navigation menu into the placeholder element
+            document.getElementById('navbar').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading the navigation menu:', error);
+        });
+}
+
+// Call the function to load the navigation menu
+loadNavigationMenu();
+
+function loadFooter() {
+    // Fetch the Footer file
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            // Insert the navigation menu into the placeholder element
+            document.getElementById('footer').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading the footer:', error);
+        });
+}
+loadFooter();
